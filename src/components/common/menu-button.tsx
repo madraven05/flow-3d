5ccdacb9e7fc7026da654ada841b896e0aebca9c
pranshu-kumar <pranshukumargond05@gmail.com>
2024-09-28 16:38:06 +0530

    grid plane added

--- src/components/common/menu-button.tsx
+++ src/components/common/menu-button.tsx
@@ -1,15 +1,15 @@
 import React, { ButtonHTMLAttributes, useState } from "react";
 
 interface MenuButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-  popoverText?: string;
+  popovertext?: string;
 }
 const MenuButton: React.FC<MenuButtonProps> = (props) => {
-  const { children, className, popoverText = "" } = props;
+  const { children, className, popovertext = "" } = props;
   const [hovered, setHovered] = useState(false);
 
   return (
     <button onPointerOver={() => setHovered(true)} onPointerLeave={() => setHovered(false)} {...props} className={`${className} disabled:text-gray-400 flex gap-5 items-center disabled:translate-y-0 hover:-translate-y-0.5 transition duration-300 ease-in-out`}>
-      <p className={`${hovered && popoverText !== "" ? 'inline' : 'hidden'} w-fit bg-gray-500/50 -left-3 px-2 py-1 text-white/70 rounded-lg absolute top-10 text-xs`}>{popoverText}</p>
+      <p className={`${hovered && popovertext !== "" ? 'inline' : 'hidden'} w-fit bg-gray-500/50 -left-3 px-2 py-1 text-white/70 rounded-lg absolute top-10 text-xs`}>{popovertext}</p>
       {children}
     </button>
   );
